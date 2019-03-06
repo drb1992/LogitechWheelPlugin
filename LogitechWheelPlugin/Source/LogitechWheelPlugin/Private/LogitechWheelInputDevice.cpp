8e2b97fd5f2213d111750663285440d4eebd5fdb
bah <email.do.bah@gmail.com>
2019-03-05 23:27:59 -0300

    Update to VS2017

--- LogitechWheelPlugin/Source/LogitechWheelPlugin/Private/LogitechWheelInputDevice.cpp
+++ LogitechWheelPlugin/Source/LogitechWheelPlugin/Private/LogitechWheelInputDevice.cpp
@@ -1,7 +1,7 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 
-#include "LogitechWheelPluginPrivatePCH.h"
 #include "LogitechWheelInputDevice.h"
+#include "LogitechWheelPluginPrivatePCH.h"
 #include "LogitechSteeringWheelLib.h"
 #include "ILogitechWheelPlugin.h"
 #include "GenericPlatform/IInputInterface.h"
